950a81bfcf5553ea7673dc692b3719addc51f442
zemihi.mathieu <mathieu_zemihi@hotmail.fr>
2023-11-08 16:43:38 +0100

    Partage initial du projet

---
+++ Room.java
@@ -0,0 +1,109 @@
+/**
+ * Class Room - a room in an adventure game.
+ *
+ * This class is part of the "World of Zuul" application. 
+ * "World of Zuul" is a very simple, text based adventure game.  
+ *
+ * A "Room" represents one location in the scenery of the game.  It is 
+ * connected to other rooms via exits.  The exits are labelled north, 
+ * east, south, west.  For each direction, the room stores a reference
+ * to the neighboring room, or null if there is no exit in that direction.
+ * 
+ * @author  Michael Kolling and David J. Barnes
+ * @version 2006.03.30
+ */
+public class Room 
+{
+    private String description;
+    private Room northExit;
+    private Room southExit;
+    private Room eastExit;
+    private Room westExit;
+    private Room upExit;
+    private Room downExit;
+
+    /**
+     * Create a room described "description". Initially, it has
+     * no exits. "description" is something like "a kitchen" or
+     * "an open court yard".
+     * @param description The room's description.
+     */
+    
+    
+    public Room ChangeRoom(String direction,Room currentRoom){
+        Room nextRoom = null;
+        if(direction.equals("north")) {
+            nextRoom = currentRoom.northExit;
+        }
+        if(direction.equals("east")) {
+            nextRoom = currentRoom.eastExit;
+        }
+        if(direction.equals("south")) {
+            nextRoom = currentRoom.southExit;
+        }
+        if(direction.equals("west")) {
+            nextRoom = currentRoom.westExit;
+        }
+        if(direction.equals("down")) {
+            nextRoom = currentRoom.downExit;
+        }
+        if(direction.equals("up")) {
+            nextRoom = currentRoom.upExit;
+        }
+        
+        return nextRoom;
+    }
+    
+    public void affichageRoom(Room currentRoom){
+            if(currentRoom.northExit != null)
+                System.out.print("north ");
+            if(currentRoom.eastExit != null)
+                System.out.print("east ");
+            if(currentRoom.southExit != null)
+                System.out.print("south ");
+            if(currentRoom.westExit != null)
+                System.out.print("west ");
+            if(currentRoom.upExit != null)
+                System.out.print("up ");
+            if(currentRoom.downExit != null)
+                System.out.print("down ");
+    }
+    
+    public Room(String description) 
+    {
+        this.description = description;
+    }
+
+    /**
+     * Define the exits of this room.  Every direction either leads
+     * to another room or is null (no exit there).
+     * @param north The north exit.
+     * @param east The east east.
+     * @param south The south exit.
+     * @param west The west exit.
+     */
+    public void setExits(Room north, Room east, Room south, Room west,Room up, Room down) 
+    {
+        if(north != null)
+            northExit = north;
+        if(east != null)
+            eastExit = east;
+        if(south != null)
+            southExit = south;
+        if(west != null)
+            westExit = west;
+        if(down != null)
+            downExit = down;
+        if(up != null)
+            upExit = up;
+    }
+
+    /**
+     * @return The description of the room.
+     */
+    public String getDescription()
+    {
+        return description;
+    }
+
+}
